268c04795b27c087c94f75496548d8b22a5c2b1b
jedisongoku <beratbekce@gmail.com>
2016-04-29 17:58:27 -0700

    Version Update

---
+++ Assets/_Scripts/Board.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour
+{
+    public static Board board;
+    public int width;
+    public int height;
+    public static GameObject[,] candies;
+    public static List<GameObject> candiesMatched;
+    public static List<int> newSpawnColumn;
+
+    private int totalCandy;
+    private int spawnCounter = 0;
+    private bool isRefilling = false;
+
+    
+
+    void Awake()
+    {
+        board = this;
+        candies = new GameObject[width,height];
+        candiesMatched = new List<GameObject>();
+        newSpawnColumn = new List<int>();
+
+        StartCoroutine("FillTheBoard");
+
+    }
+
+    IEnumerator FillTheBoard()
+    {
+        GameObject candy = Instantiate(Resources.Load("Candy"), new Vector3(totalCandy % width * 2.5f, height * 2.5f, 0), Quaternion.identity) as GameObject;
+        //Debug.Log(totalCandy % width);
+        candy.GetComponent<Candy>().columnNumber = totalCandy % width;
+        totalCandy++;
+        //candy.GetComponent<Candy>().candyNumber = totalCandy;
+        
+
+        yield return new WaitForSeconds(0.07f);
+
+        if(totalCandy < height * width)
+        {
+            StartCoroutine("FillTheBoard");
+        }
+        else
+        {
+            StopCoroutine("FillTheBoard");
+        }
+        
+    }
+
+    IEnumerator RefillTheBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            foreach(var column in newSpawnColumn)
+            {
+                if(i == column)
+                {
+                    GameObject candy = Instantiate(Resources.Load("Candy"), new Vector3(column * 2.5f, (height + 1) * 2.5f, 0), Quaternion.identity) as GameObject;
+                    candy.GetComponent<Candy>().columnNumber = column;
+                    totalCandy++;
+                    newSpawnColumn.Remove(column);
+                    break;
+                } 
+            }
+
+        }
+
+        yield return new WaitForSeconds(0.6f);
+
+        
+
+        if (newSpawnColumn.Count > 0)
+        {
+            StartCoroutine("RefillTheBoard");
+        }
+        else
+        {
+            StopCoroutine("RefillTheBoard");
+            isRefilling = false;
+            newSpawnColumn.Clear();
+            //spawnCounter = 0;
+        }
+    }
+
+    public void CandyDestroy()
+    {
+        Invoke("DestroyCandies", 0.1f);
+    }
+    void DestroyCandies()
+    {
+        //Debug.Log(candiesMatched.Count);
+        if(candiesMatched.Count >= 3)
+        {
+            foreach (var candy in candiesMatched)
+            {
+                Destroy(candy);
+            }
+            totalCandy -= candiesMatched.Count;
+
+            if (!isRefilling)
+            {
+                Debug.Log("Refilling Called!");
+                isRefilling = true;
+                StartCoroutine("RefillTheBoard");
+            }
+            //Invoke("CallRefill", 0);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/Candy.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Candy : MonoBehaviour
+{
+    public delegate void MatchCandy();
+    public static event MatchCandy OnMatch;
+
+    public List<Sprite> candySprites = new List<Sprite>();
+
+    public bool isChecked = false;
+    public bool isMatched = false;
+    public int columnNumber;
+    public int candyNumber;
+
+    private int candySpriteIndex; // sprite index
+
+    private SpriteRenderer candyRenderer;
+    private Vector2 position;
+    private List<Candy> neighbors = new List<Candy>();
+
+    void Awake()
+    {
+        //OnMatch += CandyMatch;
+        candyRenderer = GetComponent<SpriteRenderer>();
+        candySpriteIndex = Random.Range(0, candySprites.Count);
+        candyRenderer.sprite = candySprites[candySpriteIndex];
+    }
+
+    public void SetPosition(int x, int y)
+    {
+        position = new Vector2(x, y);
+    }
+
+    public int GetCandyNumber()
+    {
+        return candySpriteIndex;
+    }
+
+    void OnMouseDown()
+    {
+        Board.candiesMatched.Clear();
+        //Board.newSpawnColumn.Clear();
+
+        foreach (var candy in neighbors)
+        {
+            //Debug.Log("Neighbor " + candy.columnNumber);
+        }
+        
+        CheckNeighbor(candySpriteIndex);
+        Board.board.CandyDestroy();
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<Candy>() != null)
+        {
+            if (!neighbors.Contains(other.GetComponent<Candy>()))
+            {
+                neighbors.Add(other.GetComponent<Candy>());
+            }
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Candy>() != null)
+        {
+            if (neighbors.Contains(other.GetComponent<Candy>()))
+            {
+                neighbors.Remove(other.GetComponent<Candy>());
+            }
+        }
+    }
+
+
+    public void CheckNeighbor(int _candyNumber)
+    {
+        Board.newSpawnColumn.Add(columnNumber);
+        Board.candiesMatched.Add(gameObject);
+        //Debug.Log("Column " + columnNumber);
+        isChecked = true;
+
+        foreach (var candy in neighbors)
+        {
+            if(candy.candySpriteIndex == _candyNumber && !candy.isChecked)
+            {
+                candy.CheckNeighbor(candySpriteIndex);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    /*public void CheckNeighbor(int _candyNumber)
+    {
+        if(!isChecked)
+        {
+            isChecked = true;
+            if(candyNumber == _candyNumber)
+            {
+                isMatched = true;
+
+                if (position.x - 1 >= 0)
+                {
+                    Board.candies[(int)position.x - 1, (int)position.y].GetComponent<Candy>().CheckNeighbor(candyNumber);
+                }
+                if (position.x + 1 <= Board.board.width)
+                {
+                    Board.candies[(int)position.x + 1, (int)position.y].GetComponent<Candy>().CheckNeighbor(candyNumber);
+                }
+                if (position.y - 1 >= 0)
+                {
+                    Board.candies[(int)position.x, (int)position.y - 1].GetComponent<Candy>().CheckNeighbor(candyNumber);
+                }
+                if (position.y + 1 >= Board.board.height)
+                {
+                    Board.candies[(int)position.x, (int)position.y + 1].GetComponent<Candy>().CheckNeighbor(candyNumber);
+                }
+
+            }
+
+        }
+
+    }*/
+
+
+
+
+}
